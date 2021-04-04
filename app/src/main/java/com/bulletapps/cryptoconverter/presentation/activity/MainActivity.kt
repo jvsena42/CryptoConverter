b4b135ba39b27da3aa5aaf50639761bc7f66bc2b
João Victor Sena <jvsena16@gmail.com>
2021-04-04 09:02:34 -0300

    Initial commit

---
+++ app/src/main/java/com/bulletapps/cryptoconverter/presentation/activity/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.bulletapps.cryptoconverter.presentation.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.bulletapps.cryptoconverter.R
+import com.bulletapps.cryptoconverter.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+    }
+}
