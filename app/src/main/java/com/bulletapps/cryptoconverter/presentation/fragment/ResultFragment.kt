60cc3d212b6b0d19cbcab441538031a786f1bac7
João Victor Sena <jvsena16@gmail.com>
2021-04-08 21:32:07 -0300

    [ADD] fragment_result.xml / ResultFragment.kt - datadinding

--- app/src/main/java/com/bulletapps/cryptoconverter/presentation/fragment/ResultFragment.kt
+++ app/src/main/java/com/bulletapps/cryptoconverter/presentation/fragment/ResultFragment.kt
@@ -5,11 +5,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
 import com.bulletapps.cryptoconverter.R
+import com.bulletapps.cryptoconverter.databinding.FragmentResultBinding
 
 
 class ResultFragment : Fragment() {
 
+    private lateinit var binding: FragmentResultBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -17,6 +20,12 @@ class ResultFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_result, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentResultBinding.bind(view)
+
     }
 
 }
